ef45e4d0ef02b695b5e5e8e46bda779b0703e819
Edmundo Batista <edmundo_ab@hotmail.com>
2015-11-09 11:01:51 -0200

    Criado classe de Usuario Criado classe de RetornaUsuario

---
+++ src/main/java/java8/RetornaUsuario.java
@@ -0,0 +1,18 @@
+package java8;
+
+import java.util.List;
+import java.util.Arrays;
+
+public class RetornaUsuario {
+	public static void main(String ... args) {
+		Usuario user1 = new Usuario("Edmundo de Azevedo Batista", 120);
+		Usuario user2 = new Usuario("Walison Moreira", 1200);
+		Usuario user3 = new Usuario("Guilherme Freitas Pacheco", 900);
+		
+		List<Usuario> usuarios = Arrays.asList(user1, user2, user3);
+		
+		for (Usuario u : usuarios)  {
+			System.out.println(u.getNome());
+		}	
+	}
+}
---
+++ src/main/java/java8/Usuario.java
@@ -0,0 +1,31 @@
+package java8;
+
+public class Usuario {
+	
+	public String nome;
+	public int pontos;
+	public boolean moderador;
+	
+	public Usuario (String nome, int pontos) {
+		this.pontos = pontos;
+		this.nome = nome;
+		this.moderador = false;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public int getPontos() {
+		return pontos;
+	}
+	
+	public boolean getModerador() {
+		return moderador;
+	}
+	
+	public void tornaModerador() {
+		this.moderador = true;
+	}
+
+}
