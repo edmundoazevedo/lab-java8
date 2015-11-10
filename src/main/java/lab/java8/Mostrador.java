3a9d4c76c1c40f11e2261f27e0240ea4fda4d34c
Edmundo Batista <edmundo_ab@hotmail.com>
2015-11-10 09:38:59 -0200

    Usabilidade do lambda

--- src/main/java/lab/java8/Mostrador.java
+++ src/main/java/lab/java8/Mostrador.java
@@ -4,16 +4,24 @@ import java.util.function.Consumer;
 
 public class Mostrador implements Consumer<Usuario> {
 	
-		
-	//Consumer<Usuario> mostrador = (Usuario u) ->
-	//	System.out.println(u.getNome());
+	/*	
+	Consumer<Usuario> mostrador = (Usuario u) ->
+		System.out.println(u.getNome()); */
 	
-	//Consumer<Usuario> mostrador = (Usuario u) -> {
-	//	System.out.println(u.getNome())
-	//};
+	/*
+	  Consumer<Usuario> mostrador = (Usuario u) -> {
+		System.out.println(u.getNome())
+	}; */
 	
-	Consumer<Usuario> mostrador = u ->
-		System.out.println(u.getNome());
+	
+	  Consumer<Usuario> mostrador = u ->
+		System.out.println(u.getNome()); 
+	
+	/* 
+	 usuarios.forEach(u -> System.out.println(u.getNome()));  */
+	
+	/* usuarios.forEach(u -> u.tornaModerador()); */
+	
 
 	@Override
 	public void accept(Usuario t) {
