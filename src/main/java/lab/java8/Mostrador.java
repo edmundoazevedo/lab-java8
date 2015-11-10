067a9bf1de6a155ac9be49d5d2d1960f0f7e0280
Edmundo Batista <edmundo_ab@hotmail.com>
2015-11-10 09:12:05 -0200

    Criado classe do Mostrador para iniciar a demonstração do uso da função criado no Java8

---
+++ src/main/java/lab/java8/Mostrador.java
@@ -0,0 +1,10 @@
+package lab.java8;
+
+import java.util.function.Consumer;
+
+public class Mostrador implements Consumer<Usuario> {
+	
+	public void accept(Usuario u) {
+		System.out.println(u.getNome());
+	}	
+}
