01cd1c3b5620698a6414fc181f0a62694c479311
Edmundo Batista <edmundo_ab@hotmail.com>
2015-11-10 09:31:08 -0200

    Mostrando como a função ajudará a minimizar o código a ser criado

--- src/main/java/lab/java8/Mostrador.java
+++ src/main/java/lab/java8/Mostrador.java
@@ -4,7 +4,20 @@ import java.util.function.Consumer;
 
 public class Mostrador implements Consumer<Usuario> {
 	
-	public void accept(Usuario u) {
+		
+	//Consumer<Usuario> mostrador = (Usuario u) ->
+	//	System.out.println(u.getNome());
+	
+	//Consumer<Usuario> mostrador = (Usuario u) -> {
+	//	System.out.println(u.getNome())
+	//};
+	
+	Consumer<Usuario> mostrador = u ->
 		System.out.println(u.getNome());
-	}	
+
+	@Override
+	public void accept(Usuario t) {
+		// TODO Auto-generated method stub
+		
+	}
 }
